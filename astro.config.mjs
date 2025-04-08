289618e47d81444fba7434bb46c411a7cb7b7065
Ciro Andrea Strazzullo <ciroandreastrazzullo06@gmail.com>
2025-04-08 18:21:09 +0200

    aggiunta file documentazione

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://stackhorizon.github.io',
-	base: '/TapSystemBresso',
+	base: '/',
 	vite: {
 		plugins: [tailwindcss()],
 	},
